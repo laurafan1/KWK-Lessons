96dc557d6f0a038d76426cfac0e57423258f5208
laurafan1 <68654906+laurafan1@users.noreply.github.com>
2020-07-23 18:26:00 -0400

    Add files via upload

---
+++ StoryBoardIntro/StoryBoardIntro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoryBoardIntro
+//
+//  Created by Laura Fan on 7/22/20.
+//  Copyright © 2020 Laura Fan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print ("Hello")
+    }
+
+
+}
+
